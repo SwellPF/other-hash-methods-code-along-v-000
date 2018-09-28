70432701f4b6946797be798493d30b7af8e77da8
Paul Fell <swellpf@gmail.com>
2018-09-28 21:10:12 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -11,8 +11,9 @@ require 'pry'
 def get_the_values(groceries)
   groceries.each do |category, item_details_hash|
   item_details_hash.each do |attribute, data|
-   data
+   
     end
   end
+  data.
 end
     
